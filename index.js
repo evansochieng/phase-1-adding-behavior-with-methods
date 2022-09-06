b3588a608e918f2f4206fe2fb98b90fac2046191
Evans Ochieng <evansochieng121@gmail.com>
2022-09-06 17:46:44 +0300

    Write methods in classes

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+// Writing a method that uses instance data and parameter data
+
+// class Cat
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    // create a method
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+// class Dog
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    //create a method
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+// class Bird
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    // create a method
+    speak(){
+        if (`${this.sex}` === 'male'){
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
